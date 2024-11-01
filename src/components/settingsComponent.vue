bd831e26cb6110cdf99206900cb6b0485be2bcb6
Iliya Zaytsev <Zaycev3011@mail.ru>
2024-11-01 20:35:41 +0500

    Main page complited

---
+++ src/components/settingsComponent.vue
@@ -0,0 +1,37 @@
+<script>
+import OpenSttingsBtn from "./ui/buttons/openSttingsBtn.vue";
+
+export default {
+  components: {
+    OpenSttingsBtn,
+  },
+  methods: {
+    check() {
+      alert("check");
+    },
+  },
+};
+</script>
+
+<template>
+  <article class="settings-component">
+    <OpenSttingsBtn :openSettingsBtnFunction="check" />
+  </article>
+</template>
+
+<style scoped>
+.settings-component {
+  box-sizing: border-box;
+
+  background-color: blueviolet;
+
+  height: 300px;
+  width: 300px;
+
+  border: 4px solid #000;
+  border-left: 2px solid #000;
+  border-radius: 0px 30px 30px 0px;
+
+  display: flex;
+}
+</style>
